3a1b813e255104d446f606d2d20543950ba3b8b7
4ppolyon <romain.alves@etu.univ-grenoble-alpes.fr>
2026-01-28 04:00:21 +0100

    ajout des epsilon-transition et meilleur affichage

--- test-automate-bin.py
+++ test-automate-bin.py
@@ -24,7 +24,8 @@ class EtatAFN:
         self.nom = nom
         self.transitions = {
             '0': set(),
-            '1': set()
+            '1': set(),
+            'ε': set()
         }
         self.final = final
 
@@ -72,6 +73,70 @@ class AutomateAFN:
             t1 = ", ".join(e.nom for e in etat.transitions['1'])
             print(f"État {etat.nom}{final_str}: 0 -> {{{t0}}}, 1 -> {{{t1}}}")
         print()
+
+    def epsilon_cloture(self, etat):
+        pile = [etat]
+        cloture = {etat}
+
+        print(f"ε-clôture({etat.nom}) : ", end="")
+
+        while pile:
+            courant = pile.pop()
+            for e in courant.transitions['ε']:
+                if e not in cloture:
+                    cloture.add(e)
+                    pile.append(e)
+        nom_cloture = "{" + ",".join(e.nom for e in cloture) + "}"
+        print(nom_cloture)
+        return cloture, nom_cloture
+
+    def supprimer_epsilon(self):
+        print("\nSUPPRESSION DES ε-TRANSITIONS\n")
+
+        nouveaux_etats = {}
+
+        # Étape 1 : ε-clôtures
+        clotures = {}
+        for e in self.etats:
+            combinaison, nom_cloture = self.epsilon_cloture(e)
+            clotures[e] = {"nom" : nom_cloture, "etats": combinaison}
+            if nom_cloture not in nouveaux_etats:
+                nouveaux_etats[nom_cloture] = EtatAFN(nom=nom_cloture)
+
+        # Étape 2 : nouvelles transitions (sans ε)
+        for e in self.etats:
+            print(f"\nTraitement de l’état {e.nom}")
+
+            for symbole in ['0', '1']:
+                destination = []
+
+                print(f"  Symbole {symbole} :")
+
+                for p in clotures[e]["etats"]:
+                    print(f"    depuis {p.nom}", end="")
+                    for q in p.transitions[symbole]:
+                        # ajouter la ε-clôture de q à la destination
+                        print(f" vers la cloture de {q.nom}, qui est {clotures[q]["nom"]}", end="")
+                        destination.append(q)
+                    print()
+
+                for d in destination:
+                    nouveaux_etats[clotures[e]["nom"]].ajouter_transition(symbole, nouveaux_etats[clotures[d]["nom"]])
+
+                print("    → {" + ",".join(nouveaux_etats[clotures[d]["nom"]].nom for d in destination) + "}")
+        print()
+
+        # Étape 3 : états finaux
+        for e in self.etats:
+            if any(p.est_final() for p in clotures[e]["etats"]):
+                nouveaux_etats[clotures[e]["nom"]].final = True
+                print(f"✔ {clotures[e]["nom"]} devient FINAL")
+        print()
+
+        return AutomateAFN(
+            etats=list(nouveaux_etats[clotures[e]["nom"]] for e in self.etats),
+            etat_initial=nouveaux_etats[clotures[self.etat_initial]["nom"]]
+        )
 
     def tableau_determinisation(self):
         symboles = ['0', '1']
@@ -217,15 +282,15 @@ if __name__ == "__main__":
     automate = Automate([e0, e1, e2], e0)
     automate.afficher()
 
-    test = TestAutomate(automate=automate)
+    print("\nTESTS DE L'AUTOMATE DÉTERMINISTE\n")
 
+    test = TestAutomate(automate=automate)
     def critere_mod4_eq_1(n):
         return n % 4 == 1
-    
-    test.tester_automate(critere=critere_mod4_eq_1, n_tests=1000000, afficher_premiers=10)
+    test.tester_automate(critere=critere_mod4_eq_1, n_tests=100000, afficher_premiers=10)
 
-    print("_"*80 +"\n\nTEST DE LA DÉTERMINISATION D'UN AFN RECONNAISSANT LES CHAÎNES DE LONGUEUR ≥ 3\n"
-          "AVEC UN '1' EN ANTÉPÉNULTIME CARACTÈRE\n")
+    print("_"*180 +"\n\nTEST DE LA DÉTERMINISATION D'UN AFN RECONNAISSANT LES CHAÎNES DE LONGUEUR ≥ 3\n"
+                  "AVEC UN '1' EN ANTÉPÉNULTIME CARACTÈRE\n")
 
     # Création des états pour l'automate non déterministe
     e0 = EtatAFN("e0")
@@ -245,15 +310,105 @@ if __name__ == "__main__":
     afn = AutomateAFN([e0, e1, e2, e3], e0)
     afn.afficher()
 
+    print("\nDÉTERMINISATION DE L'AFN\n")
+
     afn.tableau_determinisation()
     afd = afn.determiniser()
     afd.afficher()
 
-    print("_"*80 +"\n\nTESTS DE L'AUTOMATE DÉTERMINISTE OBTENU APRÈS DÉTERMINISATION\n")
-    test_afd = TestAutomate(automate=afd)
+    print("\nTESTS DE L'AUTOMATE DÉTERMINISTE OBTENU APRÈS DÉTERMINISATION\n")
 
+    test_afd = TestAutomate(automate=afd)
     def critere_longueur_ge_3_antepenultieme_1(n):
         binaire = bin(n)[2:] # conversion en binaire sans le préfixe '0b'
         return len(binaire) >= 3 and binaire[-3] == '1'
+    test_afd.tester_automate(critere=critere_longueur_ge_3_antepenultieme_1, n_tests=100000, afficher_premiers=10)
 
-    test_afd.tester_automate(critere=critere_longueur_ge_3_antepenultieme_1, n_tests=1000000, afficher_premiers=10)
+    print("_"*180 +"\n\nTEST DE L'AUTOMATE NON DÉTERMINISTE AVEC ε-TRANSITIONS RECONNAISSANT LES NOMBRES DONT LA\n"
+                  "REPRÉSENTATION BINAIRE CONTIENT UNE SUITE ALTERNANTE DE '0' ET '1'\n")
+
+    # Création des états pour l'automate non déterministe avec ε-transitions
+    e0 = EtatAFN("e0")
+    e1 = EtatAFN("e1", final=True)
+    e2 = EtatAFN("e2", final=True)
+
+    # Définition des transitions pour l'automate non déterministe avec ε-transitions
+    e0.ajouter_transition('ε', e1)
+    e0.ajouter_transition('ε', e2)
+    e1.ajouter_transition('0', e2)
+    e2.ajouter_transition('1', e1)
+
+    afn_epsilon = AutomateAFN([e0, e1, e2], e0)
+    afn_epsilon.afficher()
+
+    print("\nSUPPRESSION DES ε-TRANSITIONS\n")
+
+    afn_sans_epsilon = afn_epsilon.supprimer_epsilon()
+    afn_sans_epsilon.afficher()
+
+    print("\nDÉTERMINISATION DE L'AFN SANS ε-TRANSITIONS\n")
+
+    afn_sans_epsilon.tableau_determinisation()
+    afd_final = afn_sans_epsilon.determiniser()
+    afd_final.afficher()
+
+    print("\nTESTS DE L'AUTOMATE DÉTERMINISTE OBTENU APRÈS DÉTERMINISATION ET SUPPRESSION DES ε-TRANSITIONS\n")
+
+    test_afd_final = TestAutomate(automate=afd_final)
+    def critere_alternance_01(n):
+        binaire = bin(n)[2:] # conversion en binaire sans le préfixe '0b'
+        return all(binaire[i] != binaire[i+1] for i in range(len(binaire)-1))
+    test_afd_final.tester_automate(critere=critere_alternance_01, n_tests=100000, afficher_premiers=10)
+
+    print("_"*180 +"\n\nTEST DE L'AUTOMATE NON DÉTERMINISTE AVEC ε-TRANSITIONS RECONNAISSANT LES NOMBRES DONT LA\n"
+                  "REPRÉSENTATION BINAIRE SE TERMINE PAR '011'\n")
+
+    # Création des états pour l'automate non déterministe avec ε-transitions
+    e0 = EtatAFN("0")
+    e1 = EtatAFN("1")
+    e2 = EtatAFN("2")
+    e3 = EtatAFN("3")
+    e4 = EtatAFN("4")
+    e5 = EtatAFN("5")
+    e6 = EtatAFN("6")
+    e7 = EtatAFN("7")
+    e8 = EtatAFN("8")
+    e9 = EtatAFN("9")
+    e10 = EtatAFN("10", final=True)
+
+    # Définition des transitions pour l'automate non déterministe avec ε-transitions
+    e0.ajouter_transition('ε', e1)
+    e0.ajouter_transition('ε', e6)
+    e1.ajouter_transition('ε', e2)
+    e2.ajouter_transition('0', e3)
+    e3.ajouter_transition('ε', e6)
+    e1.ajouter_transition('ε', e4)
+    e4.ajouter_transition('1', e5)
+    e5.ajouter_transition('ε', e6)
+    e6.ajouter_transition('ε', e1)
+    e6.ajouter_transition('ε', e7)
+    e7.ajouter_transition('0', e8)
+    e8.ajouter_transition('1', e9)
+    e9.ajouter_transition('1', e10)
+
+    afn_epsilon = AutomateAFN([e0, e1, e2, e3, e4, e5, e6, e7, e8, e9, e10], e0)
+    afn_epsilon.afficher()
+
+    print("\nSUPPRESSION DES ε-TRANSITIONS\n")
+
+    afn_sans_epsilon = afn_epsilon.supprimer_epsilon()
+    afn_sans_epsilon.afficher()
+
+    print("\nDÉTERMINISATION DE L'AFN SANS ε-TRANSITIONS\n")
+
+    afn_sans_epsilon.tableau_determinisation()
+    afd_final = afn_sans_epsilon.determiniser()
+    afd_final.afficher()
+
+    print("\nTESTS DE L'AUTOMATE DÉTERMINISTE OBTENU APRÈS DÉTERMINISATION ET SUPPRESSION DES ε-TRANSITIONS\n")
+
+    test_afd_final = TestAutomate(automate=afd_final)
+    def critere_fini_par_abb(n):
+        binaire = bin(n)[2:] # conversion en binaire sans le préfixe '0b'
+        return binaire.endswith('011')
+    test_afd_final.tester_automate(critere=critere_fini_par_abb, n_tests=100000, afficher_premiers=10)
